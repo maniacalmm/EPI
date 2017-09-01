c58ea955ee6bfd6af1a754ffea46ce9866ca98cb
Tang <out_tang_look@outlook.com>
2017-09-01 15:55:09 +0900

    Dijkstra partition, quick 3 way

--- src/Ch5Arrays.java
+++ src/Ch5Arrays.java
@@ -1,4 +1,6 @@
-public class Arrays {
+import java.util.*;
+
+public class Ch5Arrays {
     public static void evenOdd(int[] A) {
         // given an array of int, put the even number first
         // the advantage of array is that we can efficiently operate from both ends
@@ -15,8 +17,49 @@ public class Arrays {
         }
     }
 
+/***************************************************/
+    public static enum Color {RED, BLACK, BLUE, fuckingwhite};
 
+    public static void dutchFlagParition1(int pivotIndex, List<Color> A) {
+        Color p = A.get(pivotIndex);
+        // First pass: group elements smaller than pivot
+        int smaller = 0;
+        for (int i = 0; i < A.size(); i++) {
+            if (A.get(i).ordinal() < p.ordinal())
+                Collections.swap(A, smaller++, i);
+        }
 
+        //Second pass: group elements larger than pivot
+        int larger = A.size() - 1;
+        for (int j = A.size() - 1; j >= 0 && A.get(j).ordinal() >= p.ordinal(); j--) {
+            if (A.get(j).ordinal() > p.ordinal())
+                Collections.swap(A, larger--, j);
+        }
+    }
+
+    public static void dutchFlagPartition2(int pivotIndex, List<Color> A) {
+        int smaller = 0, equal = 0, larger = A.size();
+        Color p = A.get(pivotIndex);
+        while(equal < larger) {
+            if (A.get(equal).ordinal() < p.ordinal())
+                Collections.swap(A, smaller++, equal++);
+            else if (A.get(equal).ordinal() > p.ordinal())
+                Collections.swap(A, --larger, equal++);
+            else equal++;
+        }
+    }
+
+/***************************************************/
+
+public static void main(String[] args) {
+    List<Color> a = new ArrayList<>();
+    a.add(Color.BLUE);
+    a.add(Color.RED);
+    a.add(Color.fuckingwhite);
+    for (Color e : a)
+        System.out.println(e.ordinal());
+
+}
 
 
 }
