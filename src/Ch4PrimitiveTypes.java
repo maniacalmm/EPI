d0e04a27f9d65db7d76c502f704d2c5679e75acd
Tang <out_tang_look@outlook.com>
2017-09-03 11:46:30 +0900

    progressing

--- src/Ch4PrimitiveTypes.java
+++ src/Ch4PrimitiveTypes.java
@@ -13,6 +13,79 @@ public class Ch4PrimitiveTypes {
         return 1L;
     }
 
+    /**************************************************************/
+    public static long multiply(long x, long y) {
+        long sum = 0;
+        while( x != 0) {
+            if ((x & 1) != 0) {
+                sum = add(sum, y);
+            }
+
+            x >>>= 1;
+            y <<= 1;
+        }
+        return sum;
+    }
+
+    private static long add(long a, long b) {
+        // unfinished, don't want to yet.
+        long sum = 0, carryin = 0, k = 1, tmpA = a, tmpB = b;
+        return sum;
+    }
+    /**************************************************************/
+
+    public static double power(double x, int y) {
+        double result = 1.0;
+        long power = y;
+        if (y < 0) {
+            power = -power;
+            x = 1.0 / x;
+        }
+
+        while(power != 0) {
+            if ((power & 1) != 0) {
+                result *= x;
+            }
+
+            x *= x;
+            power >>>= 1;
+        }
+        return result;
+    }
+
+    /**************************************************************/
+    public static long reverse(int x) {
+        long result = 0;
+        long xRemaining = Math.abs(x);
+        while(xRemaining > 0) {
+            result = 10 * result + xRemaining % 10;
+            xRemaining /= 10;
+        }
+
+        return x > 0? result : -result;
+    }
+
+    /**************************************************************/
+    public static boolean isPalindromeNumber(int x) {
+        if (x <= 0) { // negative numbers are not Palindrome
+            return x == 0;
+        }
+
+        final int numDigits = (int) (Math.floor(Math.log10(x))) + 1; // I used to do this as well
+        int msdMask = (int) Math.pow(10, numDigits - 1);
+
+        for (int i = 0; i < (numDigits / 2); i++) {
+            if (x / msdMask != x % 10)
+                return false;
+            x %= msdMask;
+            x /= 10;
+            msdMask /= 100;
+        }
+
+        return true;
+    }
+
+    /**************************************************************/
     public static void main(String[] args) {
         int input = 0b101000101011111;
         Ch4PrimitiveTypes sb = new Ch4PrimitiveTypes();
--- src/Ch5Arrays.java
+++ src/Ch5Arrays.java
@@ -50,16 +50,124 @@ public class Ch5Arrays {
     }
 
 /***************************************************/
+    public static List<Integer> plusOne(List<Integer> A) {
+        int n = A.size() - 1;
+        A.set(n, A.get(n) + 1); // increase the LSB
 
-public static void main(String[] args) {
-    List<Color> a = new ArrayList<>();
-    a.add(Color.BLUE);
-    a.add(Color.RED);
-    a.add(Color.fuckingwhite);
-    for (Color e : a)
-        System.out.println(e.ordinal());
+        for (int i = n; i > 0 && A.get(i) != 10; i--) {
+            A.set(i, 0);
+            A.set(i - 1, A.get(i - 1) + 1);
+        }
 
-}
+        if (A.get(0) == 10) {
+            A.set(0, 1);
+            A.add(0);
+        }
+        return A;
+    }
+
+    public static String binaryAdd(String A, String B) {
+        // 011 001
+        int nA = A.length() - 1;
+        int nB = B.length() - 1;
+
+        if (nA > nB) {
+            for (int i = nA - nB; i > 0; i--)
+                B = "0" + B;
+        } else if (nA < nB) {
+            for (int j = nB - nA; j > 0; j--)
+                A = "0" + A;
+        }
+
+        System.out.println(A + " " + B);
+
+        String result = "";
+        boolean cin = false;
+        for (int i = nA; i >= 0; i--) {
+            //System.out.println("before: result " + result + " cin: " + cin);
+            if (A.charAt(i) == B.charAt(i)) {
+                if (A.charAt(i) == '1') {
+                    if (cin) result = "1" + result;
+                    else result = "0" + result;
+
+                    cin = true;
+                } else {
+                    if (cin) result = "1" + result;
+                    else result = "0" + result;
+
+                    cin = false;
+                }
+            } else {
+                if (cin) {
+                    result = "0" + result;
+                    cin = true;
+                } else {
+                    result = "1" + result;
+                    cin = false;
+                }
+            }
+            //System.out.println("after: result " + result + " cin: " + cin);
+        }
+
+        if (cin) result = "1" + result;
+        return result;
+    }
+
+
+/***************************************************/
+    public static List<Integer> multiply(List<Integer> num1, List<Integer> num2) {
+        final int sign = (num1.get(0) < 0) ^ (num2.get(0) < 0) ? -1 : 1;
+        num1.set(0, Math.abs(num1.get(0)));
+        num2.set(0, Math.abs(num2.get(0)));
+
+        List<Integer> result =
+                new ArrayList<>(Collections.nCopies(num1.size() + num2.size(), 0));
+
+        for (int e : result)
+            System.out.print(e);
+        System.out.println();
+
+        for (int i = num1.size() - 1; i >= 0; i--) {
+            for (int j = num2.size() - 1; j >= 0; j--) {
+                result.set(i + j + 1, result.get(i + j + 1) + num1.get(i) * num2.get(j));
+                result.set(i + j, result.get(i + j) + result.get(i + j + 1) / 10);
+                result.set(i + j + 1, result.get(i + j + 1) % 10);
+            }
+        }
+
+        // Remove the leading zeros;
+        int firstNotZero = 0;
+        while(firstNotZero < result.size() && result.get(firstNotZero) == 0)
+            firstNotZero++;
+        result = result.subList(firstNotZero, result.size());
+        if (result.isEmpty()) {
+            return  Arrays.asList(0);
+        }
+
+        result.set(0, result.get(0) * sign);
+        return result;
+    }
+
+/***************************************************/
+
+    public static void main(String[] args) {
+        //List<Color> a = new ArrayList<>();
+        //a.add(Color.BLUE);
+        //a.add(Color.RED);
+        //a.add(Color.fuckingwhite);
+        //System.out.println(binaryAdd("111111", "001"));
+        //System.out.println();
+        Integer[] a = new Integer[] {1,2,3,4,5,6};
+        Integer[] b = new Integer[] {6,5,4,3,2,1};
+        List<Integer> A = Arrays.asList(a);
+        List<Integer> B = Arrays.asList(b);
+
+        for (int e : multiply(A, B))
+            System.out.print(e);
+        System.out.println();
+
+
+    }
 
 
 }
