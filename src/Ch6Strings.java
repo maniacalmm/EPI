4d15dd2bbb644aaa40e4c579555627e7ee9cdd2c
Tang <out_tang_look@outlook.com>
2017-09-07 16:58:59 +0900

    progressing

--- src/Ch6Strings.java
+++ src/Ch6Strings.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Ch6Strings {
     public static String intToString(int x) {
         boolean isNegative = false;
@@ -72,15 +75,112 @@ public class Ch6Strings {
         return s.toString();
     }
 
+    public static int ssDecodeColID(String x) {
+        int result = 0;
+        for (int i = 0; i < x.length(); i++) {
+            int tmp = x.charAt(i) - 'A' + 1;
+            result = tmp + result * 26;
+        }
 
+        return result;
+    }
+
+
+    public static String replaceAndRemoveBook(int size, char[] s) {
+        // EPI does this double iteration, first pass from the beginning they deleted every b
+        // and the second iteration from the end they replaced every a with double d
+        //       a c b b a a
+        //       _ _ c _ _ _ _
+
+        // remove every b and count a
+
+        StringBuilder result = new StringBuilder();
+        int writeIdx = 0, aCount = 0;
+        for (int i = 0; i < size; i++) {
+            if (s[i] != 'b') s[writeIdx++] = s[i];
+            if (s[i] == 'a') aCount++;
+        }
+
+       //Backward iteration: replace a with dd starting from the end
+        int curIdx = writeIdx - 1;
+        writeIdx = writeIdx + aCount - 1;
+        final int finalSize = writeIdx + 1;
+
+        while(curIdx >= 0) {
+            if(s[curIdx] == 'a') {
+                s[writeIdx--] = 'd';
+                s[writeIdx--] = 'd';
+            } else {
+                s[writeIdx--] = s[curIdx];
+            }
+
+            --curIdx;
+        }
+
+        for (int i = 0; i < s.length && s[i] != 0; i++) {
+            result.append((char) s[i]);
+        }
+
+        return result.toString();
+    }
+
+    public static String printArray(char[] s) {
+        String str = "";
+
+        for (int i = 0; i < s.length && s[i] != 0; i++) {
+            str += s[i];
+        }
+
+        return str;
+    }
+
+    public static boolean isPalindrome(String s) {
+        int start = 0, end = s.length() - 1;
+
+        while(start < end) {
+            while (isLetter(s.charAt(start)))
+                start++;
+            while(isLetter(s.charAt(end)))
+                end--;
+
+            System.out.println(s.charAt(start) + " " + s.charAt(end));
+            if (s.charAt(start) != s.charAt(end) &&
+                    s.charAt(start) - 32 != s.charAt(end) &&
+                    s.charAt(start) + 32 != s.charAt(end))
+                return false;
+            start++; end--;
+        }
+
+        return true;
+    }
+
+    private static boolean isLetter(char c) {
+        if (! ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <='z')))
+            return true;
+        return false;
+    }
 
     public static void main(String[] args) {
         String b = "12345";
         int a = 38472034;
 
+        char[] s = new char[20];
+        s[0] = 'a';
+        s[1] = 'c';
+        s[2] = 'b';
+        s[3] = 'b';
+        s[4] = 'a';
+        s[5] = 'a';
+
+        char[] s1 = Arrays.copyOf(s,  s.length);
+
         //System.out.println(intToString(a));
         //System.out.println(stringToInt(b));
 
-        System.out.println(convertBase("615", 7, 13));
+        //System.out.println(convertBase("615", 7, 13));
+        //System.out.println(ssDecodeColID("ZZ"));
+        System.out.println(replaceAndRemoveBook(6, s));
+
+        System.out.println(isPalindrome("A man, a plan, a canal, Panama."));
     }
 }
