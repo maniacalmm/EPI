402c581f19b2afbc6d8799b52600fab983df718f
Tang <out_tang_look@outlook.com>
2017-09-19 13:22:35 +0900

    heap done, nlog(k), fun

--- src/Ch10Heaps.java
+++ src/Ch10Heaps.java
@@ -60,4 +60,186 @@ public class Ch10Heaps {
 
         return result;
     }
+
+
+    private static enum SubarrayType {INCREASING, DECREASING}
+
+    public static List<Integer> sortKthIncreasingDecreasingArray(List<Integer> A) {
+        List<List<Integer>> sortedSubArrays = new ArrayList<>();
+        SubarrayType subarrayType = SubarrayType.INCREASING;
+        int startIdx = 0;
+        for (int i = 1; i <= A.size(); i++) {
+            if (i == A.size() || (A.get(i - 1) < A.get(i) && subarrayType == SubarrayType.DECREASING)
+                              || (A.get(i) > A.get(i - 1) && subarrayType == SubarrayType.INCREASING)) {
+                List<Integer> subList = A.subList(startIdx, i); // sublist i, exclusive
+                if (subarrayType == SubarrayType.DECREASING) {
+                    Collections.reverse(subList);
+                }
+                sortedSubArrays.add(subList);
+                subarrayType = (subarrayType == SubarrayType.DECREASING
+                                                ? SubarrayType.INCREASING
+                                                : SubarrayType.DECREASING);
+            }
+        }
+
+        return mergeSortedArrays(sortedSubArrays);
+    }
+
+    // a partially sorted array with a length of K, the moral of the story is that, we can sort it with
+    // nlog(k) instead of nlog(n), and if k << n, then we can improve the performance
+    public static void sortApproximatelySortedData(Iterator<Integer> sequence, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // default priority queue is minHeap, natural order
+
+        for (int i = 0; i < k && sequence.hasNext(); i++) {
+            minHeap.add(sequence.next());
+        }
+
+        while(sequence.hasNext()) {
+            minHeap.add(sequence.next());
+            Integer smallest = minHeap.remove(); // remove head, kinda like poll()
+            System.out.println(smallest);
+        }
+
+        while(!minHeap.isEmpty()) System.out.println(minHeap.remove());
+    }
+
+    public static class Star implements Comparable<Star> {
+        private double x, y, z;
+
+        public Star (double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        private double distance() {
+            return Math.sqrt(x*x + y*y + z*z);
+        }
+
+        @Override
+        public int compareTo(Star other) {
+            return Double.compare(this.distance(), other.distance());
+        }
+    }
+
+    public static List<Star> findClosestKStars(int k, Iterator<Star> stars) {
+        PriorityQueue<Star> maxHeap = new PriorityQueue<>(k, Collections.reverseOrder());
+        while(stars.hasNext()) {
+            maxHeap.add(stars.next());
+            if (maxHeap.size() == k + 1) maxHeap.remove();
+        }
+
+        List<Star> result = new ArrayList<>();
+        while(!maxHeap.isEmpty()) result.add(maxHeap.remove());
+        Collections.sort(result);
+        return result;
+    }
+
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+    public static void onlineMedian(Iterator<Integer> sequence) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(DEFAULT_INITIAL_CAPACITY,Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(DEFAULT_INITIAL_CAPACITY);
+
+        while(sequence.hasNext()) {
+            minHeap.add(sequence.next());
+
+            if (minHeap.size() - maxHeap.size() >= 2) {
+                maxHeap.add(minHeap.remove());
+            }
+            double Median = minHeap.size() == maxHeap.size()
+                    ? (double) ((minHeap.peek() + maxHeap.peek())) / 2.0
+                    : (minHeap.peek());
+            System.out.println("Median: " + Median);
+        }
+
+    }
+
+    private static class HeapEntry {
+        public Integer index;
+        public Integer value;
+
+        public HeapEntry(Integer index, Integer value) {
+            this.index = index;
+            this.value = value;
+        }
+    }
+
+
+    private static class Compare implements Comparator<HeapEntry> {
+        @Override
+        public int compare(HeapEntry o1, HeapEntry o2) {
+            return Integer.compare(o1.value, o2.value);
+        }
+
+        public static final Compare COMPARE_HEAP_ENTRIES = new Compare(); // actually kind of a cool tricks
+    }
+
+    // a heap for heap, a heap within another heap
+    public static List<Integer> KLargestInBinaryHeap(List<Integer> A, int k) {
+        // A is the list representation of a maxHeap
+        if (k <= 0) return Collections.EMPTY_LIST;
+
+        PriorityQueue<HeapEntry> candidateMaxHeap = new PriorityQueue<>(
+                DEFAULT_INITIAL_CAPACITY, Compare.COMPARE_HEAP_ENTRIES);
+        candidateMaxHeap.add(new HeapEntry(0, A.get(0)));
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            Integer candidiateIdx = candidateMaxHeap.peek().index;
+            result.add(candidateMaxHeap.remove().value);
+
+            // the problem is that we only know that the upper level is greater or equal compares to the lower ones
+            // by introducing this heap thing, we can differentiate between left and right
+            Integer left = candidiateIdx * 2 + 1; // zero based index
+            if (left < A.size()) candidateMaxHeap.add(new HeapEntry(left, A.get(left)));
+            Integer right = candidiateIdx * 2 + 2;
+            if (right < A.size()) candidateMaxHeap.add(new HeapEntry(right, A.get(right)));
+        }
+
+        return result;
+    }
+
+    private static class ValueWithRank {
+        public Integer value;
+        public Integer rank;
+
+        public ValueWithRank(Integer value, Integer rank) {
+            this.value = value;
+            this.rank = rank;
+        }
+    }
+
+
+    public static class Compare1 implements Comparator<ValueWithRank> {
+        @Override
+        public int compare(ValueWithRank o1, ValueWithRank o2) {
+            Integer.compare(o2.rank, o1.rank); // this is the reverse maxHeap order
+        }
+
+        public static final Compare1 COMPARE_VALUEWITHRANK = new Compare1();
+    }
+
+    public static class StackUsingHeap {
+        private int timeStamp = 0;
+
+        private PriorityQueue<ValueWithRank> maxHeap = new PriorityQueue<>(Compare1.COMPARE_VALUEWITHRANK);
+
+        public void push(Integer x) {
+            maxHeap.add(new ValueWithRank(x, timeStamp++));
+        }
+
+        public Integer pop() {
+            return maxHeap.remove().value;
+        }
+
+        public Integer peek() {
+            return maxHeap.peek().value;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> runningMedian = Arrays.asList(1,0,3,5,2,0,1);
+        onlineMedian(runningMedian.iterator());
+    }
+
+
 }
